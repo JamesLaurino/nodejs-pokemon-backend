3cdcdcbb714c0f0c39e2e087cf4345f1c64ef656
jamesLaurino <petersthomas90@gmail.com>
2025-05-18 15:07:20 +0200

    env var ajout

--- src/db/Sequelize.js
+++ src/db/Sequelize.js
@@ -4,6 +4,12 @@ const UserModel = require('../models/user');
 
 const pokemons = require('./Mock-pokemon');
 
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+const DB_DIALECT = process.env.DB_DIALECT;
+
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
     dialect: DB_DIALECT
